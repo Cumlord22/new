facbbe9a6c71abf1fe8711397ce1dee3f7eb91db
HiepTrinh_ <88824625+Cumlord22@users.noreply.github.com>
2022-03-31 00:57:54 +0700

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,30 @@
+package bank.t2108a;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Controller {
+
+    private static long balanceNumber = 1000000;
+    List<TransactionHistory> listHistory = new ArrayList<TransactionHistory>();;
+
+    public long getBalanceNumber(){
+        return balanceNumber;
+    }
+
+    public static long transfer (long money){
+        return balanceNumber;
+    }
+
+    public void actionTransfer(){
+    }
+
+    public void getHistory(){
+    }
+
+    public static String formatMoney(long money){
+        DecimalFormat formatter = new DecimalFormat("###,###,##0.00");
+        return formatter.format(money);
+    }
+}
